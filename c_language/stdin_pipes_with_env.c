7af1a2db33ba0c19bfbe49c682c1d85afcf47c27
Brian Chau <brian.chau@outlook.com>
2022-06-04 17:18:22 -0700

    Updated C programs

--- c_language/stdin_pipes_with_env.c
+++ c_language/stdin_pipes_with_env.c
@@ -4,21 +4,20 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int fxn(char *prod_app, char * cons_app, char** prod_args, char** cons_args) {
+int pwncollege(char *prod_app, char * cons_app, char** prod_args, char** cons_args, char **prod_env, char **cons_env) {
     int i = 0, j = 0;
 
     if ( 0 == (i = fork()) ) {                            // Child1 process
         int pipe_fds[2];
         pipe(pipe_fds);
         if ( 0 == (j = fork()) ) {                        // Child1 > Child2 forked process
-            //sleep(1);                                   // Simulate wait
             dup2(pipe_fds[STDOUT_FILENO], STDOUT_FILENO); // Use the WRITE pipe for the "Child1 > Child2" forked process
             close(pipe_fds[STDIN_FILENO]);                // Close the READ pipe for the "Child1 > Child2" forked process, since it's not used
-            execve(prod_app, prod_args, NULL);
+            execve(prod_app, prod_args, prod_env);
         } else {                                          // Child1 > Parent2 forked process
             dup2(pipe_fds[STDIN_FILENO], STDIN_FILENO);   // Use the READ pipe for the "Child1 > Parent2" forked process
             close(pipe_fds[STDOUT_FILENO]);               // Close the WRITE pipe for the "Child1 > Parent2" forked process, since it's not used
-            execve(cons_app, cons_args, NULL);
+            execve(cons_app, cons_args, cons_env);
         }
     } else {                                              // Parent1 process
         waitpid(i, NULL, 0);
@@ -28,10 +27,13 @@ int fxn(char *prod_app, char * cons_app, char** prod_args, char** cons_args) {
 
 int main(int argc, char** argv) {
     char *prod_app    = "/usr/bin/cat";
-    char *prod_args[] = { prod_app, "file.txt", (char *) NULL };
+    char *prod_args[] = { prod_app, "uuwrolou", (char *) NULL };
+    char *prod_env[] = { (char*) NULL };
 
-    char *cons_app    = "/path/to/bin";
-    char *cons_args[] = { cons_app, (char *) NULL };
-    fxn(prod_app, cons_app, prod_args, cons_args);
+    char *cons_app    = "/challenge/embryoio_level32";
+    char *cons_args[] = { cons_app, "xmnocmwmow", (char *) NULL };
+    char *cons_env[] = { "tglarq=aoatbfyuqn", (char*) NULL };
+
+    pwncollege(prod_app, cons_app, prod_args, cons_args, prod_env, cons_env);
     return 0;
 }
